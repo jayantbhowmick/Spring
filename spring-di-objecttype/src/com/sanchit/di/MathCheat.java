252d491964bf0b1eab712bda71f65c61106d52e6
Atharva Naphade <naphade21@gmail.com>
2022-10-09 14:42:46 +0530

    Update MathCheat.java

--- spring-di-objecttype/src/com/sanchit/di/MathCheat.java
+++ spring-di-objecttype/src/com/sanchit/di/MathCheat.java
@@ -6,5 +6,6 @@ package com.sanchit.di;
 public class MathCheat {
     public void mathCheat() {
         System.out.println("math cheating started... ");
+        System.out.println("math cheating started... ");
     }
 }
